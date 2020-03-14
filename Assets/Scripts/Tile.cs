f3ae5307b7be0d3db60526958e686ac1aebceefd
Buzzy <sorin_buzatu@yahoo.com>
2020-03-14 17:19:09 +0200

    tile generator

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Tile m_TileUp;
+    public Tile m_TileDown;
+    public Tile m_TileLeft;
+    public Tile m_TileRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
