5ca254dc8910c78607b8a8ecb47cfdabb1a470be
Tovarasul <tonyfrum@yahoo.com>
2020-06-01 22:58:51 +0300

    added hearts

---
+++ Assets/Scripts/UI/Hp_UI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hp_UI : MonoBehaviour
+{
+    [SerializeField]
+    List<Transform> m_HeartPrefab;
+    List<Transform> m_Hearts;
+    Dictionary<string, Transform> m_PrefabDictionary;
+    float c_Decrement = 1.2f;
+
+    public static Hp_UI instance;
+
+    private void Awake()
+    {
+        m_Hearts = new List<Transform>();
+        m_PrefabDictionary = new Dictionary<string, Transform>();
+
+        foreach(Transform prefab in m_HeartPrefab)
+        {
+            m_PrefabDictionary.Add(prefab.name, prefab);
+        }
+
+        if (instance != null)
+        {
+            Debug.LogWarning("Instanta deja creata!");
+            Destroy(instance);
+        }
+        instance = GetComponent<Hp_UI>();
+    }
+
+    public void DrawHearts(float _currentHP, int _totalHP)
+    {
+        foreach(Transform heart in m_Hearts)
+        {
+            Destroy(heart.gameObject);
+        }
+        m_Hearts.Clear();
+        float positionX = 2.4f;
+        float positionY = 0.5f;
+        string heartName = "hp_full";
+        for (int i = 1; i <= _totalHP; ++i)
+        {
+            if (i - 0.5f == _currentHP)
+            {
+                heartName = "hp_half";
+            }
+            Transform heart = Instantiate(m_PrefabDictionary[heartName], transform);
+            heart.localPosition = new Vector3(positionX, positionY, 0);
+            m_Hearts.Add(heart);
+
+            positionX -= c_Decrement;
+            if (i == 5)
+            {
+                positionX = 2.4f;
+                positionY = -0.5f;
+            }
+            if (i >= _currentHP)
+            {
+                heartName = "hp_empty";
+            }
+        }
+    }
+}
--- Assets/Scripts/_Tests/UI/Test_UI.cs
+++ Assets/Scripts/_Tests/UI/Test_UI.cs
@@ -18,5 +18,25 @@ public class Test_UI : MonoBehaviour
         {
             UI.instance.NotifyUIChange("attack", "long_sword");
         }
+        if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            Hp_UI.instance.DrawHearts(1, 1);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha5))
+        {
+            Hp_UI.instance.DrawHearts(3, 7);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha6))
+        {
+            Hp_UI.instance.DrawHearts(6.5f, 7);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha7))
+        {
+            Hp_UI.instance.DrawHearts(10, 10);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha8))
+        {
+            Hp_UI.instance.DrawHearts(1.5f, 10);
+        }
     }
 }
