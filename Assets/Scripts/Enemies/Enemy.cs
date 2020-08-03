296ba5ffac0b255338a93f1e9dd024aa90ede067
Andreea <andreeaoprescu12@yahoo.com>
2020-08-03 09:12:55 +0300

    enemies are on the correct layer now

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : AboveTileObject, IBeat
         //m_SubscriberList = new List<IPlayerSubscriber>(); This is shitty; Trebuie un loader
         Map.instance.AddEnemy(GetComponent<Enemy>());
         Vector3 new_position = m_CurrentTile.transform.position;
-        transform.position = new Vector3(new_position.x, new_position.y, -2f);
+        transform.position = new Vector3(new_position.x, new_position.y, m_CurrentTile.GetLayerNumber());
 
         StartEnemy();
     }
