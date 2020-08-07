cd1af48112201386310a05e28f674ddfca51b696
Andreea <andreeaoprescu12@yahoo.com>
2020-08-07 09:28:03 +0300

    added more impact to digging: camera shake + sounds

--- Assets/Scripts/Sound/SoundReferences/SoundEffects.cs
+++ Assets/Scripts/Sound/SoundReferences/SoundEffects.cs
@@ -8,10 +8,13 @@ public class SoundEffects : GenericSoundReference
     List<AudioClip> m_PlayerHit;
     [SerializeField]
     List<AudioClip> m_GoldPickup;
+    [SerializeField]
+    List<AudioClip> m_Dig;
 
     void Awake()
     {
         m_AudioClipDictionary.Add("player_hit", m_PlayerHit);
         m_AudioClipDictionary.Add("pickup_gold", m_GoldPickup);
+        m_AudioClipDictionary.Add("dig", m_Dig);
     }
 }
